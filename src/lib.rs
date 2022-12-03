19e29b2f07fe3951b2d6401be9018db1c0365b20
Andrew Dirksen <andrew@dirksen.com>
2022-12-03 14:36:10 -0800

    Make Register packet generic over backing so it can have mutable versions, should able to have an owning version too.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod signatures;
 
 use std::{
+    borrow::{Borrow, BorrowMut},
     convert::TryInto,
     net::{IpAddr, Ipv6Addr, SocketAddr},
 };
@@ -26,29 +27,24 @@ impl<'a> Forward<'a> {
 }
 
 #[derive(PartialEq, Eq, Debug)]
-pub struct Register<'a> {
-    bs: &'a [u8; Register::REGISTER_SIZE],
+pub struct Register<T> {
+    bs: T,
 }
 
-impl<'a> Register<'a> {
-    const PUBLIC_KEY_START: usize = 0;
-    const PUBLIC_KEY_END: usize = Self::PUBLIC_KEY_START + 32;
-    const SOCKET_ADDRESS_START: usize = Self::PUBLIC_KEY_END;
-    const SOCKET_ADDRESS_END: usize = Self::SOCKET_ADDRESS_START + 18;
-    const TIMESTAMP_START: usize = Self::SOCKET_ADDRESS_END;
-    const TIMESTAMP_END: usize = Self::TIMESTAMP_START + 16;
-    const SIGNATURE_START: usize = Self::TIMESTAMP_END;
-    const SIGNATURE_END: usize = Self::TIMESTAMP_END + 64;
-    const REGISTER_SIZE: usize = Self::SIGNATURE_END;
+const PUBLIC_KEY_START: usize = 0;
+const PUBLIC_KEY_SIZE: usize = 32;
+const SOCKET_ADDRESS_START: usize = PUBLIC_KEY_START + PUBLIC_KEY_SIZE;
+const SOCKET_ADDRESS_SIZE: usize = 18;
+const TIMESTAMP_START: usize = SOCKET_ADDRESS_START + SOCKET_ADDRESS_SIZE;
+const TIMESTAMP_SIZE: usize = 16;
+const SIGNATURE_START: usize = TIMESTAMP_START + TIMESTAMP_SIZE;
+const SIGNATURE_SIZE: usize = 64;
+const REGISTER_SIZE: usize = SIGNATURE_START + SIGNATURE_SIZE;
 
-    /// return the Forward type packet or None if the input
-    /// is not the right size
-    pub fn parse(bs: &'a [u8]) -> Option<Self> {
-        Some(Self {
-            bs: bs.try_into().ok()?,
-        })
-    }
-
+impl<T> Register<T>
+where
+    T: Borrow<[u8; REGISTER_SIZE]>,
+{
     /// Check whether the signature is valid
     pub fn verify_signature(&self) -> bool {
         // we should let register just be a reference to an array
@@ -56,45 +52,71 @@ impl<'a> Register<'a> {
         valid(self.address(), self.signed_message(), self.signature())
     }
 
-    pub fn address(&self) -> PublicKey<'a> {
-        PublicKey(
-            self.bs[Self::PUBLIC_KEY_START..Self::PUBLIC_KEY_END]
-                .try_into()
-                .unwrap(),
-        )
+    fn range<const START: usize, const LEN: usize>(&self) -> &[u8; LEN] {
+        range::<u8, START, LEN, REGISTER_SIZE>(self.bs.borrow())
+    }
+
+    pub fn address(&self) -> PublicKey {
+        PublicKey(self.range::<PUBLIC_KEY_START, PUBLIC_KEY_SIZE>())
     }
 
     pub fn socket_address(&self) -> SocketAddr {
-        parse_socketaddr(
-            self.bs[Self::SOCKET_ADDRESS_START..Self::SOCKET_ADDRESS_END]
-                .try_into()
-                .unwrap(),
-        )
+        parse_socketaddr(self.range::<SOCKET_ADDRESS_START, SOCKET_ADDRESS_SIZE>())
     }
 
     pub fn timestamp_nanos(&self) -> u128 {
-        u128::from_be_bytes(
-            self.bs[Self::TIMESTAMP_START..Self::TIMESTAMP_END]
-                .try_into()
-                .unwrap(),
-        )
+        u128::from_be_bytes(*self.range::<TIMESTAMP_START, TIMESTAMP_SIZE>())
     }
 
-    pub fn signature(&self) -> Signature<'a> {
-        Signature(
-            self.bs[Self::SIGNATURE_START..Self::SIGNATURE_END]
-                .try_into()
-                .unwrap(),
-        )
+    pub fn signature(&self) -> Signature {
+        Signature(self.range::<SIGNATURE_START, SIGNATURE_SIZE>())
     }
 
-    pub fn signed_message(
-        &self,
-    ) -> &'a [u8; Register::TIMESTAMP_END - Register::SOCKET_ADDRESS_START] {
-        self.bs[Self::SOCKET_ADDRESS_START..Self::TIMESTAMP_END]
-            .try_into()
-            .unwrap()
+    pub fn signed_message(&self) -> &[u8; SIGNATURE_START - SOCKET_ADDRESS_START] {
+        self.range::<SOCKET_ADDRESS_START, { SIGNATURE_START - SOCKET_ADDRESS_START }>()
     }
+}
+
+impl<T> Register<T>
+where
+    T: BorrowMut<[u8; REGISTER_SIZE]>,
+{
+    fn range_mut<const START: usize, const LEN: usize>(&mut self) -> &mut [u8; LEN] {
+        range_mut::<u8, START, LEN, REGISTER_SIZE>(self.bs.borrow_mut())
+    }
+
+    pub fn set_address(&mut self, address: PublicKey) {
+        *self.range_mut::<PUBLIC_KEY_START, PUBLIC_KEY_SIZE>() = *address.0;
+    }
+
+    pub fn set_socket_address(&self, socket_address: SocketAddr) {
+        todo!()
+    }
+
+    pub fn set_timestamp_nanos(&self, timestamp_nanos: u128) {
+        todo!()
+    }
+
+    pub fn set_signature(&self, signature: Signature) {
+        todo!()
+    }
+}
+
+fn range<T, const START: usize, const LEN: usize, const CONTAINER: usize>(
+    bs: &[T; CONTAINER],
+) -> &[T; LEN] {
+    assert!(START + LEN <= CONTAINER);
+    bs[START..(START + LEN)].try_into().unwrap()
+}
+
+fn range_mut<T, const START: usize, const LEN: usize, const CONTAINER: usize>(
+    bs: &mut [T; CONTAINER],
+) -> &mut [T; LEN] {
+    assert!(START + LEN <= CONTAINER);
+    bs.get_mut(START..(START + LEN))
+        .unwrap()
+        .try_into()
+        .unwrap()
 }
 
 fn parse_socketaddr(dat: &[u8; 18]) -> SocketAddr {
@@ -112,6 +134,10 @@ fn parse_socketaddr(dat: &[u8; 18]) -> SocketAddr {
     let port = u16::from_be_bytes(port);
 
     SocketAddr::new(addr, port)
+}
+
+fn write_socketaddr(sa: SocketAddr) -> [u8; 18] {
+    todo!()
 }
 
 fn take_array_ref<T, const N: usize>(slice: &[T]) -> Option<(&[T; N], &[T])> {
@@ -158,7 +184,7 @@ mod tests {
     #[test]
     fn parse_register() {
         #[rustfmt::skip]
-        let packet = [
+        let packet: [u8; REGISTER_SIZE] = [
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // ] public key
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // ]
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // ]
@@ -181,7 +207,7 @@ mod tests {
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // ]
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, // ]
         ];
-        let r = Register::parse(&packet).unwrap();
+        let r = Register { bs: &packet };
         assert_eq!(
             r.address(),
             PublicKey(&[
